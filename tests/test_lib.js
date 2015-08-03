f656392a6c1db755adc5e61a724a2de85b4863be
rockdragon <moyerock@gmail.com>
2015-08-03 18:12:51 +0800

    img/urf: OK.

--- tests/test_lib.js
+++ tests/test_lib.js
@@ -1,6 +1,6 @@
 var img2urf = require("../lib/lib_img2urf");
 
-img2urf('../samples/sample.gif', '../samples/sample.urf', function(err){
+img2urf.convert('../samples/sample.gif', '../samples/sample.urf', function(err){
     if(err) return console.log(err);
     console.log("DONE");
 });
